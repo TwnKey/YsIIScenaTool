4988b096e2b503f6a9a66d076d83edcfd69568c6
TwnKey <feitaishi45@gmail.com>
2023-05-29 13:14:30 +0200

    parsing done?

--- YsIIScenaTool/YsIIScenaTool.cpp
+++ YsIIScenaTool/YsIIScenaTool.cpp
@@ -14,7 +14,12 @@ uint32_t read_u32(const std::vector<uint8_t>& content, uint32_t &addr) {
 
 
 }
-
+uint16_t read_u16(const std::vector<uint8_t>& content, uint32_t& addr) {
+	uint16_t out;
+	memcpy(&out, content.data() + addr, sizeof(uint16_t));
+	addr += sizeof(uint16_t);
+	return out;
+}
 std::string read_str(const std::vector<uint8_t>& content, uint32_t &addr) {
 	std::vector<uint8_t> temp = {};
 
@@ -50,8 +55,6 @@ uint16_t read_u16_at(const std::vector<uint8_t>& content, uint32_t addr) {
 	uint16_t out;
 	memcpy(&out, content.data() + addr, sizeof(uint16_t));
 	return out;
-
-
 }
 //Copied from Darkmet's text decryption
 std::vector<uint8_t> decrypt(int key, std::vector<uint8_t>& content)
@@ -112,10 +115,11 @@ int main(int argc, char* argv[])
 			uint32_t ptr = read_u32(decrypted_content, current_addr) + addr_sect_2_ptr_end;
 			uint8_t op_code = decrypted_content[ptr];
 			uint8_t cntt; 
+			uint16_t wd;
 			std::string text;
-			
+			std::cout << "new function " << std::endl;
 			while (op_code != 1) {
-				std::cout << std::hex << ptr << " " << (int)op_code << std::endl;
+				std::cout << std::hex << ptr << " OPCODE: " << (int)op_code << std::endl;
 				ptr++;
 				switch (op_code) {
 					case 0x02:
@@ -124,23 +128,40 @@ int main(int argc, char* argv[])
 					case 0x05:
 					case 0x06:
 					case 0x07:
+					case 0x08:
 					case 0x0A:
+					case 0x15:
 					case 0x17:
 					case 0x18:
+					case 0x21:
 					case 0x3E:
 					case 0x62:
+					case 0x63:
+					case 0x64:
 					case 0x67:
+					case 0x6C:
+					case 0x79:
+					case 0x7A:
 					case 0x83:
+					case 0xA0:
+					case 0xA9:
+					case 0xD2:
+					case 0xD3:
 					case 0xD5:
 					case 0xD7:
 						ptr += 2;
 						break;
+					case 0x09:
 					case 0x0C:
 					case 0x0D:
 					case 0x0E:
 					case 0x10:
 					case 0x14:
+					case 0x1A:
 					case 0x1E:
+					case 0x16:
+					case 0x19:
+					case 0x1C:
 					case 0x37:
 					
 					case 0x4F:
@@ -156,20 +177,33 @@ int main(int argc, char* argv[])
 					case 0x49:
 					case 0x4A:
 					case 0x4B:
+					case 0x56:
+					case 0x5E:
 					case 0x5F:
+					case 0x6B:
 					case 0x73:
 					case 0x74:
+					case 0xC7:
+					case 0xC8:
 					case 0xD1:
 						break;
 					case 0x38:
 					case 0x39:
 					case 0x4C:
 					case 0x4D:
+					case 0x8F:
+					case 0xA3:
+					case 0xB0:
 					case 0xB6:
+					case 0x9E:
 					case 0x9F:
+					case 0xAC:
 					case 0xB1:
+					case 0xB5:
 					case 0xB8:
+					case 0xBD:
 					case 0xBE:
+					case 0xD6:
 						ptr += 3;
 						break;
 					case 0xB2:
@@ -177,7 +211,9 @@ int main(int argc, char* argv[])
 						break;
 					case 0x3A:
 					case 0x3D:
+					case 0x58:
 					case 0xB7:
+					case 0xC1:
 						ptr += 1;
 						break;
 					case 0xBB:
@@ -187,51 +223,124 @@ int main(int argc, char* argv[])
 						text = read_str(decrypted_content, ptr);
 						output_csv << std::hex << ptr << ";\"" << text << "\";\"\";\"\";" << std::endl;
 						break;
-					
+					case 0x11:
+					case 0x13:
+					case 0x55:
+					case 0x60:
+					case 0x61:
 					case 0x6F:
+					case 0xB3:
+					case 0xB4:
+					case 0xD0:
 						cntt = decrypted_content[ptr++];
 						for (unsigned int i = 0; i < cntt; i++)
 							ptr+=2;
 						break;
 					case 0x8E:
 					case 0x5D:
-						cntt = decrypted_content[ptr++];
+					
+						cntt = decrypted_content[ptr++]-1;
 						ptr += 2;
 						for (unsigned int i = 0; i < cntt; i++)
 							ptr += 2;
 						break;
-					
 					case 0x0F:
+					case 0x12:
+					case 0x34:
+					case 0x3F:
+					case 0x57:
+					case 0x5B:
+					case 0x5C:
+					case 0x69:
+					case 0x6A:
 					case 0x70:
+					case 0x71:
 					case 0x75:
 					case 0x77:
+					case 0x78:
+					case 0x7B:
 					case 0x7C:
+					case 0x7E:
+					case 0x7F:
+					case 0x84:
 					case 0x87:
 					case 0x88:
+					case 0x8A:
+					case 0x8B:
+					case 0x8C:
 					case 0x8D:
+					case 0x91:
 					case 0x92:
 					case 0x94:
+					
+					case 0xA2:
+					case 0xA6:
+					case 0xC9:
+					case 0xCA:
+					case 0xD9:
 						ptr += 4;
 						break;
+					case 0x98:
+					case 0x99:
+					case 0xB9:
+						ptr += 5;
+						break;
 					case 0x22:
+					case 0x31:
+					case 0x50:
+					case 0x35:
+					case 0x68:
 					case 0x72:
+					case 0x90:
+					case 0x93:
+					case 0x97:
+					case 0x9B:
 					case 0xA4:
+					case 0xDA:
 						ptr += 6;
 						break;
 					case 0x95:
+					case 0xAB:
 					case 0xC2:
+					case 0xCB:
+					case 0xCC:
 						ptr += 8;
 						break;
+					case 0x25:
+					case 0x26:
 					case 0x27:
 					case 0x53:
 					case 0x80:
 						ptr += 10;
+						break;
+					case 0x81:
+					case 0xAA:
+						ptr += 12;
+						break;
+					case 0x9D:
+						ptr += 14;
 						break;
 					case 0x59:
 						ptr += 16;
 						break;
 					case 0xCD:
 						ptr += 18;
+						break;
+					case 0xCE:
+						ptr += 20;
+						break;
+					case 0xC3:
+						ptr += 28;
+						break;
+					case 0x7D:
+						cntt = (decrypted_content[ptr++]- 3 >> 1) + 1;
+						ptr += 4;
+						for (unsigned int i = 0; i < cntt; i++)
+							ptr += 4;
+						break;
+					case 0x9A:
+						ptr += 3;
+						text = read_str(decrypted_content, ptr);
 						break;
 					case 0xDB:
 						text = read_str(decrypted_content, ptr);
@@ -243,38 +352,54 @@ int main(int argc, char* argv[])
 						output_csv << std::hex << ptr << ";\"" << text << "\";\"\";\"\";" << std::endl;
 						ptr += 2;
 						break;
+					case 0x76:
+						cntt = decrypted_content[ptr++];
+						for (unsigned int i = 0; i < cntt;) {
+							wd = read_u16(decrypted_content, ptr);
+							if (wd == 0) {
+								ptr += 4;
+								i += 3;
+							}
+							else {
+								i += 2;
+								ptr += 2;
+
+								if (wd == 3) {
+									ptr += 4;
+									i += 4;
+								}
+								else if (wd == 2) {
+									ptr += 4;
+									i += 4;
+								}
+								else {
+								
+								}
+							
+							}
+						}
+							
+						break;
 					default: 
-						std::cout << std::hex << ptr << " dec " << std::dec << (int)op_code << " hex " << std::hex << (int)op_code << std::endl;
-						std::sort(op_codes.begin(), op_codes.end());
-						op_codes.erase(std::unique(op_codes.begin(), op_codes.end()), op_codes.end());
-						float prog = 100* (float)op_codes.size() / 220;
-						std::cout << op_codes.size() << " " << prog << "%" << std::endl;
- 						throw(std::exception("unknown op code"));
+						if (op_code > 220)
+						{
+						}
+						else{
+							std::cout << std::hex << ptr << " dec " << std::dec << (int)op_code << " hex " << std::hex << (int)op_code << std::endl;
+							std::sort(op_codes.begin(), op_codes.end());
+							op_codes.erase(std::unique(op_codes.begin(), op_codes.end()), op_codes.end());
+							float prog = 100* (float)op_codes.size() / 220;
+							std::cout << op_codes.size() << " " << prog << "%" << std::endl;
+ 							throw(std::exception("unknown op code"));
+						}
 					{}
 				}
 				
 				op_codes.push_back(op_code);
 				op_code = decrypted_content[ptr];
 			}
-			
-			
-			/*if (id >= 0x01 && id <= (0x01 + 0x1D)) { //equipment
-				std::string text = read_str_at(decrypted_content, ptr + 1);
-				//std::cout << std::hex << ptr << " equipment " << id << " " << text  << std::endl;
-			}
-
-			if (id >= 0x1F && id <= (0x1F + 0x29)) { //items ; 0x1E is hardcoded in the exe
-				std::string text = read_str_at(decrypted_content, ptr + 1);
-				//std::cout << std::hex << ptr << " item " << id << " " << text << std::endl;
-			}*/
-			//std::cout << std::hex << ptr << " " << id << std::endl;
 			id++;
 			
-		}
-		for (auto op : op_codes) {
-		
-			std::cout << std::hex << (int) op << std::endl;
-		
 		}
 		current_addr = addr_sect_5_start;
 		while (current_addr < addr_sect_5_ptr_end) {
